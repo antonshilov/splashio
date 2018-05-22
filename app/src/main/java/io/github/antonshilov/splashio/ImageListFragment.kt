30ecba112861599ca9e06d345518850f84fc0e6f
Anton Shilov <vaccum121@gmail.com>
2018-05-22 23:58:51 +0300

    Add Navigation arch component

---
+++ app/src/main/java/io/github/antonshilov/splashio/ImageListFragment.kt
@@ -0,0 +1,108 @@
+package io.github.antonshilov.splashio
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_image_list.*
+import timber.log.Timber
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [ImageListFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [ImageListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class ImageListFragment : Fragment() {
+    private lateinit var vm: PhotoListViewModel
+
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_image_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        vm = ViewModelProviders.of(this).get(PhotoListViewModel::class.java)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadImages()
+    }
+
+    private fun loadImages() {
+        vm.photos.observe(this, Observer { showImages(it!!) })
+        vm.loadPhotos()
+    }
+
+    private fun showImages(images: List<Photo>) {
+        Timber.d(images.toString())
+        imageGrid.adapter = PhotoAdapter(this.context!!, images)
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ImageListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = ImageListFragment()
+    }
+}
--- app/src/main/java/io/github/antonshilov/splashio/MainActivity.kt
+++ app/src/main/java/io/github/antonshilov/splashio/MainActivity.kt
@@ -1,32 +1,20 @@
 package io.github.antonshilov.splashio
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var vm: PhotoListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        vm = ViewModelProviders.of(this).get(PhotoListViewModel::class.java)
-        loadImages()
-    }
-
-
-    private fun loadImages() {
-        vm.photos.observe(this, Observer { showImages(it!!) })
-        vm.loadPhotos()
-    }
-
-    private fun showImages(images: List<Photo>) {
-        Timber.d(images.toString())
-        imageGrid.adapter = PhotoAdapter(this, images)
+//        savedInstanceState?.let {
+//            supportFragmentManager.beginTransaction()
+//                    .replace(R.id.fragment_nav_host, ImageListFragment.newInstance())
+//                    .commitNow()
+//
+//        }
     }
 
 
