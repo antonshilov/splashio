c1cfb138fea940d5e9e8cd33220f50cf34d5d80b
Anton <vaccum121@gmail.com>
2018-05-23 22:10:59 +0300

    Feature/arch components (#1)
    * Migrate to Architecture Components and Studio 3.2
    
    * Fix imports and build file after experiments with androidx
    
    * Add Navigation arch component
    
    * Keep image aspect ratio in the list

---
+++ app/src/main/java/io/github/antonshilov/splashio/PhotoListViewModel.kt
@@ -0,0 +1,43 @@
+package io.github.antonshilov.splashio
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PhotoListViewModel : ViewModel() {
+
+    val photos: MutableLiveData<List<Photo>> = MutableLiveData()
+    val loader: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun loadPhotos() {
+        loadStatus(true)
+        val unsplashApi = Retrofit.Builder()
+                .baseUrl(UnsplashService.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(UnsplashService::class.java)
+        val call = unsplashApi.getFeed()
+        call.enqueue(object : Callback<List<Photo>> {
+            override fun onFailure(call: Call<List<Photo>>, t: Throwable) {
+                loadStatus(false)
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onResponse(call: Call<List<Photo>>, response: Response<List<Photo>>) {
+                loadStatus(false)
+                val images = response.body()!!
+                photos.value = images
+            }
+
+        })
+    }
+
+    private fun loadStatus(isLoading: Boolean) {
+        loader.value = isLoading
+    }
+
+}
---
+++ app/src/main/java/io/github/antonshilov/splashio/SplashioApp.kt
@@ -0,0 +1,12 @@
+package io.github.antonshilov.splashio
+
+import android.app.Application
+import timber.log.Timber
+
+class SplashioApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+
+    }
+}
