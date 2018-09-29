fd8408e25d5948992f5f5efdc6716e0543482cdb
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-09-29 19:02:36 +0300

    Correct dimensions, GetCollections method buildObservable() protected -> public

--- domain/src/main/java/io/github/antonshilov/domain/feed/collections/GetCollections.kt
+++ domain/src/main/java/io/github/antonshilov/domain/feed/collections/GetCollections.kt
@@ -7,7 +7,7 @@ import io.reactivex.Observable
 
 class GetCollections(private val repo: CollectionsRepo, postExecutionThread: PostExecutionThread) :
   ObservableUseCase<List<Collection>, PaginationParams>(postExecutionThread) {
-  override fun buildObservable(params: PaginationParams): Observable<List<Collection>> {
+  public override fun buildObservable(params: PaginationParams): Observable<List<Collection>> {
     return repo.getCollections(params.page, params.pageSize)
   }
 }
