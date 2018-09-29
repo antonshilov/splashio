8f5b704f7c1b2b38057e65a28a27d24ef8442525
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-09-29 19:48:44 +0300

    Add CollectionEntityMapper test

--- remote/src/test/java/io/github/antonshilov/remote/CollectionEntityMapperTest.kt
+++ remote/src/test/java/io/github/antonshilov/remote/CollectionEntityMapperTest.kt
@@ -7,6 +7,11 @@ import io.github.antonshilov.remote.mapper.ProfileImageMapper
 import io.github.antonshilov.remote.mapper.UrlMapper
 import io.github.antonshilov.remote.mapper.UserEntityMapper
 import io.github.antonshilov.remote.mapper.UserLinksMapper
+import io.github.antonshilov.remote.model.CollectionModel
+import io.github.benas.randombeans.api.EnhancedRandom
+
+import org.junit.Assert
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -16,4 +21,15 @@ class CollectionEntityMapperTest {
   private val userMapper = UserEntityMapper(ProfileImageMapper(), UserLinksMapper())
   private val photoMapper = PhotoEntityMapper(userMapper, UrlMapper(), PhotoLinksMapper())
   private val collectionMapper = CollectionEntityMapper(photoMapper)
+
+  @Test
+  fun `should map model to entity`() {
+    val model = EnhancedRandom.random(CollectionModel::class.java)
+    val entity = collectionMapper.mapFromRemote(model)
+
+    Assert.assertEquals(model.id, entity.id)
+    Assert.assertEquals(model.description, entity.description)
+    Assert.assertEquals(model.previewPhotos?.map{ it.urls.small }, entity.previewPhotosUrls)
+    Assert.assertEquals(model.user?.name, entity.userName)
+  }
 }
